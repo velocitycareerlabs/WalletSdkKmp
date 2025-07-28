9fe41aa94f235ad608e8d5033f735a3cfa1186df
Michael Avoyan <64565419+michaelavoyan@users.noreply.github.com>
2025-07-28 15:34:04 +0300

    support JS platform configuration

--- velocity-exchange-verifiers/build.gradle.kts
+++ velocity-exchange-verifiers/build.gradle.kts
@@ -56,6 +56,12 @@ kotlin {
         }
     }
 
+    js(IR) {
+        browser()
+        nodejs()
+        binaries.executable()
+    }
+
     // Source set declarations.
     // Declaring a target automatically creates a source set with the same name. By default, the
     // Kotlin Gradle Plugin creates additional source sets that depend on each other, since it is
