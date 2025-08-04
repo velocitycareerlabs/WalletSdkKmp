d4df1f3442363c7fb2bdcbda5ef3fa206b41303d
Michael Avoyan <64565419+michaelavoyan@users.noreply.github.com>
2025-08-04 17:47:52 +0300

    cont packege.json customization for JS lib

--- velocityexchangeverifiers/build.gradle.kts
+++ velocityexchangeverifiers/build.gradle.kts
@@ -72,11 +72,11 @@ kotlin {
         outputModuleName = publishArtifactId // Set module name (lowercase to avoid Node warnings) [oai_citation:1‡dev.to](https://dev.to/touchlab/different-ways-to-distribute-and-integrate-kotlinjs-library-1hg3#:~:text=Note%20that%20node%20module%20cannot,target%20to%20avoid%20that)
         binaries.library() // Build as a library (produces .js/.mjs + .d.ts + package.json) [oai_citation:2‡dev.to](https://dev.to/touchlab/different-ways-to-distribute-and-integrate-kotlinjs-library-1hg3#:~:text=As%20mentioned%20above%20in%20the,%60package.json)
 
-//        TODO: handle the following commented code
-//        compilations["main"].packageJson {
-//            name = "@velocitycareerlabs/$publishArtifactId"
-//            version = publishVersion
-//        }
+//        TODO: Discuss with Andres
+        compilations["main"].packageJson {
+            name = "@velocitycareerlabs/$publishArtifactId"
+            version = publishVersion
+        }
     }
     sourceSets.all {
         languageSettings.optIn("kotlin.js.ExperimentalJsExport") // Opt-in to @JsExport (since it's experimental)
