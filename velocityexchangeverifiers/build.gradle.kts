5f91060d73d199f8a8791329ae69ea08064f7b14
Michael Avoyan <64565419+michaelavoyan@users.noreply.github.com>
2025-08-10 16:12:31 +0300

    try hardcoded props

--- velocityexchangeverifiers/build.gradle.kts
+++ velocityexchangeverifiers/build.gradle.kts
@@ -12,10 +12,9 @@ plugins {
     alias(libs.plugins.dokka)
 }
 
-// ----- Artifact coordinates -----
-val publishGroupId = providers.gradleProperty("PUBLISH_GROUP_ID").get()
-val publishArtifactId = providers.gradleProperty("PUBLISH_ARTIFACT_ID").get()
-val publishVersion = providers.gradleProperty("PUBLISH_VERSION").get()
+val publishGroupId = "io.velocitycareerlabs" // providers.gradleProperty("PUBLISH_GROUP_ID").get()
+val publishArtifactId = "velocityexchangeverifiers" // providers.gradleProperty("PUBLISH_ARTIFACT_ID").get()
+val publishVersion = "0.1.0" // providers.gradleProperty("PUBLISH_VERSION").get()
 
 group = publishGroupId
 version = publishVersion
@@ -39,19 +38,19 @@ kotlin {
         namespace = "$publishGroupId.$publishArtifactId"
 
         // read from a single source of truth
-        compileSdk =
-            providers
-                .gradleProperty("ANDROID_COMPILE_SDK")
-                .map(String::toInt)
-//            .orElse(35)
-                .get()
+        compileSdk = 36
+//            providers
+//                .gradleProperty("ANDROID_COMPILE_SDK")
+//                .map(String::toInt)
+// //            .orElse(35)
+//                .get()
 
-        minSdk =
-            providers
-                .gradleProperty("ANDROID_MIN_SDK")
-                .map(String::toInt)
-//            .orElse(24)
-                .get()
+        minSdk = 24
+//            providers
+//                .gradleProperty("ANDROID_MIN_SDK")
+//                .map(String::toInt)
+// //            .orElse(24)
+//                .get()
 
         withHostTestBuilder { }
 
